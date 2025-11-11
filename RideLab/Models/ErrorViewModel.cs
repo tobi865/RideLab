e3590d1f7bf51a7d68c9c18124635dc9aec1f4d4
andyr <andyr@ANDYPC>
2025-11-11 21:24:59 +0200

    Add project files.

---
+++ RideLab/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace RideLab.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
